b1757f7af5d727a88f80a088b602e85decdc2d2e
nliapis <nliapis1983@gmail.com>
2015-06-21 19:58:54 +0300

    index.js

---
+++ server/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+var auth = require('./auth.js');
+var products = require('./products.js');
+var user = require('./users.js');
+
+/*
+ * Routes that can be accessed only for authenticated users 
+ */
+router.get('/api/v1/products', products.getAll);
+router.get('/api/v1/product/:id', products.getOne);
+router.post('/api/v1/product/', products.create);
+router.put('/api/v1/product/:id', products.update);
+router.delete('/api/v1/product/:id', products.delete);
+
+/*
+ * Routes that can be accessed only for authenticated & authorized users 
+ */
+router.get('/api/v1/admin/users', user.getAll);
+router.get('/api/v1/admin/user/:id', user.getOne);
+router.post('/api/v1/admin/user/', user.create);
+router.put('/api/v1/admin/user/:id', user.update);
+router.delete('/api/v1/admin/user/:id', user.delete);
+
+module.exports = router;
+
